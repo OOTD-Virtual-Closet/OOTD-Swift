39a29562e631e60159e4bd8445d5732d6895a238
sanjhee gupta <gupta.sanjhee@gmail.com>
2024-02-17 15:19:30 -0500

    done with basic settings UI

---
+++ OOTD-Swift/OOTD-Swift/Screens/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  OOTD-Swift
+//
+//  Created by Sanjhee Gupta on 2/16/24.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
