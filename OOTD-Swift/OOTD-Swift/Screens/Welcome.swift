e0b7ad11a487920dc55ff1b8790669302e09ac86
sanjhee gupta <gupta.sanjhee@gmail.com>
2024-02-21 01:25:28 -0500

    navigation in welcome screens, add new welcome screen in beginning

---
+++ OOTD-Swift/OOTD-Swift/Screens/Welcome.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome.swift
+//  OOTD-Swift
+//
+//  Created by Sanjhee Gupta on 2/21/24.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome()
+}
