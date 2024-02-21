fb0e27cfdc0631348e344e3d315df7db49c2a0bd
sanjhee gupta <gupta.sanjhee@gmail.com>
2024-02-21 01:30:06 -0500

    add changePassword UI and navigation

---
+++ OOTD-Swift/OOTD-Swift/Screens/ChangePassword.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePassword.swift
+//  OOTD-Swift
+//
+//  Created by Sanjhee Gupta on 2/21/24.
+//
+
+import SwiftUI
+
+struct ChangePassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePassword()
+}
