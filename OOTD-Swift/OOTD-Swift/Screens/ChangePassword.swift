48cc4256db51596487b64d6f1248464a38b85a72
sanjhee gupta <gupta.sanjhee@gmail.com>
2024-02-21 01:31:31 -0500

    issue with earlier commit (again (again))

--- OOTD-Swift/OOTD-Swift/Screens/ChangePassword.swift
+++ OOTD-Swift/OOTD-Swift/Screens/ChangePassword.swift
@@ -2,17 +2,108 @@
 //  ChangePassword.swift
 //  OOTD-Swift
 //
-//  Created by Sanjhee Gupta on 2/21/24.
+//  Created by Sanjhee Gupta on 2/19/24.
 //
 
 import SwiftUI
 
 struct ChangePassword: View {
+    @State private var oldPass: String = ""
+    @State private var newPass: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color.white.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack {
+                    HStack {
+                        Text("Change Password!")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundStyle(Color(hex:"CBC3E3"))
+                    }
+                    .padding()
+                    .padding()
+                    
+                    VStack {
+                        HStack {
+                            TextField("Old Password...", text: $oldPass)
+                            Image(systemName: "checkmark")
+                                .fontWeight(.bold)
+                                .foregroundColor(.green)
+                        }
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(lineWidth: 2.0)
+                                .foregroundColor(Color(hex:"898989"))
+                        )
+                        .padding()
+                        
+                        HStack {
+                            TextField("New Password...", text: $newPass)
+                            Image(systemName: "checkmark")
+                                .fontWeight(.bold)
+                                .foregroundColor(.green)
+                        }
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(lineWidth: 2.0)
+                                .foregroundColor(Color(hex:"898989"))
+                        )
+                        .padding()
+                        
+                        NavigationLink(destination: WelcomePage3()) {
+                            //TODO: REDIRECT TO FORGOT PASSWORD ONCE CREATED
+                            
+                            Text("Forgot Password?")
+                                .foregroundStyle(Color(hex: "CBC3E3"))
+                                .fontWeight(.heavy)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .padding(.trailing, 20)
+                                .padding(.bottom, 30)
+                        }
+                        .padding(.horizontal)
+                        
+                    }
+                    
+                    VStack {
+                        NavigationLink(destination: ProfileSummary(profile: Profile.default)) {
+                            //TODO: CHANGE PROFILE PARAMETERS W/ FIREBASE
+                            Text("BACK")
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .frame(height:50)
+                            .foregroundColor(.black)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                            .background(Color("UIpurple"))
+                            .cornerRadius(10)
+                        }
+                        .padding(.horizontal)
+                        
+                        NavigationLink(destination: Login()) {
+                            Text("LOGIN")
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .frame(height:50)
+                            .foregroundColor(.black)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                            .background(Color("UIpurple"))
+                            .cornerRadius(10)
+                        }
+                        .padding(.horizontal)
+                    }
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    ChangePassword()
+
+struct Change_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangePassword()
+    }
 }
