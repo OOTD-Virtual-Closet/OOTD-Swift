e63e555a785ad5b1e157a01f457861d6c4281c88
Atharva Gupta <atharva.gu@gmail.com>
2024-02-28 22:00:30 -0500

    Fixed some errors and updated Trending UI

--- OOTD-Swift/OOTD-Swift/Screens/Dashboard/MarketDashboard/TrendingView.swift
+++ OOTD-Swift/OOTD-Swift/Screens/Dashboard/MarketDashboard/TrendingView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct TrendingView: View {
     var body: some View {
-        Text("trending")
+        VStack {
+            Text("Trending 🔥")
+                .font(.system( size: 25))
+                .fontWeight(.heavy)
+                .padding(.top, 10)
+            Text("36 items")
+                .foregroundColor(.gray)
+                .font(.system( size: 13))
+                .fontWeight(.heavy)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 15)
+                .padding(.vertical, 5)
+                .padding(.top, 5)
+                .padding(.leading, 10)
+            HStack {
+                Image(systemName: "cart.fill")
+                Text("Shopping Cart")
+                    .foregroundColor(.black)
+                    .font(.system( size: 18))
+                    .fontWeight(.heavy)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 15)
+            }
+            .padding(.leading, 35)
+            Spacer()
+            .padding(.top, 20)
+        }
     }
 }
 
