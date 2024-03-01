2f96ba205b7d20adec4af63bc39e0a00148480ae
Atharva Gupta <atharva.gu@gmail.com>
2024-02-29 19:44:30 -0500

    Frontend for Recommended

--- OOTD-Swift/OOTD-Swift/Screens/Dashboard/MarketDashboard/RecommendedView.swift
+++ OOTD-Swift/OOTD-Swift/Screens/Dashboard/MarketDashboard/RecommendedView.swift
@@ -10,24 +10,22 @@ import SwiftUI
 struct ClothingItemElements: Identifiable {
     var id: UUID = UUID()
     var name: String
+    var descrption: String
     var image: String
     var price: String
     var description: String
+    var color: String
 }
 
 struct RecommendedView: View {
     let trendingItems: [ClothingItemElements] = [
-        ClothingItemElements(name: "Clothing 1", image: "clothing1", price: "$49.99", description: "A cool piece of clothing."),
-        ClothingItemElements(name: "Clothing 2", image: "clothing2", price: "$59.99", description: "Another cool piece of clothing."),
-        ClothingItemElements(name: "Clothing 3", image: "clothing3", price: "$39.99", description: "Yet another cool piece of clothing.")
+        ClothingItemElements(name: "Clothing 1", descrption: "Otton shirt. Classic sollar. Short sleeve. Cuts at the Bottoms. Button close on the front.", image: "clothing1", price: "$49.99", description: "A cool piece of clothing.", color: "#33673B"),
+        ClothingItemElements(name: "Clothing 2",descrption: "Otton shirt. Classic sollar. Short sleeve. Cuts at the Bottoms. Button close on the front.", image: "clothing2", price: "$59.99", description: "Another cool piece of clothing.", color: "#3E8989"),
+        ClothingItemElements(name: "Clothing 3",descrption: "Otton shirt. Classic sollar. Short sleeve. Cuts at the Bottoms. Button close on the front.", image: "clothing3", price: "$39.99", description: "Yet another cool piece of clothing.", color: "#D1B490")
     ]
     var body: some View {
         ScrollView {
             VStack {
-                Text("Recommended")
-                    .font(.system( size: 25))
-                    .fontWeight(.heavy)
-                    .padding(.top, 8)
                 Text("36 items")
                     .foregroundColor(.gray)
                     .font(.system( size: 13))
@@ -35,7 +33,7 @@ struct RecommendedView: View {
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.horizontal, 15)
                     .padding(.leading, 10)
-                    .padding(.top, 5)
+                    .padding(.top, 10)
                 HStack {
                     Image(systemName: "cart.fill")
                     Text("Shopping Cart")
@@ -153,17 +151,110 @@ struct ClothingDetailSheet: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 300, height: 300)
             
-            Text(item.name)
-                .font(.title)
-                .padding()
+            HStack {
+                Text(item.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .padding()
+                Text(item.price)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
             
-            Text(item.price)
-                .font(.headline)
-                .foregroundColor(.secondary)
-            
-            Text(item.description)
-                .padding()
-            
+            HStack {
+                Text(item.descrption)
+                    .fontWeight(.medium)
+                    .padding(.leading, 10)
+                Spacer()
+            }
+            HStack {
+                Text("Color")
+                    .font(.title3)
+                    .fontWeight(.heavy)
+                    .padding()
+                Spacer()
+            }
+            HStack {
+                Circle()
+                    .fill(Color(hex: item.color))
+                    .frame(width: 24, height: 24) //
+                    .padding()
+                Spacer()
+            }
+            HStack {
+                Circle()
+                    .foregroundColor(.gray)
+                    .overlay(
+                        Text("XS")
+                            .foregroundColor(.black)
+                            .font(.caption)
+                    )
+                    .frame(width: 40, height: 40)
+                    .padding()
+                Circle()
+                    .foregroundColor(.gray)
+                    .overlay(
+                        Text("S")
+                            .foregroundColor(.black)
+                            .font(.caption)
+                    )
+                    .frame(width: 40, height: 40)
+                    .padding()
+                Circle()
+                    .foregroundColor(.gray)
+                    .overlay(
+                        Text("M")
+                            .foregroundColor(.black)
+                            .font(.caption)
+                    )
+                    .frame(width: 40, height: 40)
+                    .padding()
+                Circle()
+                    .foregroundColor(.gray)
+                    .overlay(
+                        Text("L")
+                            .foregroundColor(.black)
+                            .font(.caption)
+                    )
+                    .frame(width: 40, height: 40)
+                    .padding()
+                Circle()
+                    .foregroundColor(.gray)
+                    .overlay(
+                        Text("XL")
+                            .foregroundColor(.black)
+                            .font(.caption)
+                    )
+                    .frame(width: 40, height: 40)
+                    .padding()
+                Spacer()
+            }
+            HStack {
+                Button(action: {
+                    print("Cancel")
+                }) {
+                    Text("Cancel")
+                        .foregroundColor(.black)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 10)
+                        .background(Color(hex:"EF5B5B"))
+                        .cornerRadius(15)
+                }
+                .padding(.top, 10)
+                .padding(.leading, 10)
+                Button(action: {
+                    print("Add to Cart")
+                }) {
+                    Text("Add to Cart")
+                        .foregroundColor(.black)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 10)
+                        .background(Color(hex:"FFBA49"))
+                        .cornerRadius(15)
+                }
+                .padding(.top, 10)
+                .padding(.leading, 10)
+            }
             Spacer()
         }
         .padding()
