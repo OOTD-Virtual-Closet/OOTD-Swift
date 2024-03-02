fad66ae36ebbe904712d31d76129aa89819077de
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2024-03-02 12:46:37 -0500

    add UI to change confirm deletion, and connect with email verification

--- OOTD-Swift/OOTD-Swift/Screens/DeleteAccount.swift
+++ OOTD-Swift/OOTD-Swift/Screens/DeleteAccount.swift
@@ -13,6 +13,14 @@ enum deleteAccountErrors: Error{
     case incorrectEmail
 }
 
+
+@MainActor
+final class ProfileViewModelConfirm: ObservableObject {
+    func verifyDelete() throws {
+        try AuthManager.shared.verifyDelete()
+    }
+}
+
 @MainActor
 final class ProfileViewModelDeleteAcc: ObservableObject {
     func GetAuthenticatedUser() throws -> AuthDataResultModel {
@@ -20,9 +28,7 @@ final class ProfileViewModelDeleteAcc: ObservableObject {
         return user
     }
     
-    func verifyDelete() throws {
-        try AuthManager.shared.verifyDelete()
-    }
+   
 
     func deleteAcc(emailP1: String, emailP2: String) async throws {
         
@@ -50,10 +56,52 @@ final class ProfileViewModelDeleteAcc: ObservableObject {
             return false
         }
     }
-    
-    
 }
 
+struct ConfirmDelete: View {
+    @StateObject private var viewModel = ProfileViewModelConfirm()
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("are you sure?")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(Color(hex:"CBC3E3"))
+                
+                NavigationLink(destination: DeleteAccountUI()) {
+                    Text("NO, BACK")
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .frame(height:50)
+                        .foregroundColor(.black)
+                        .fontWeight(.bold)
+                        .padding(.horizontal)
+                        .background(Color("UIpurple"))
+                        .cornerRadius(10)
+                        
+                }
+                
+                Button {
+                    Task {
+                        try viewModel.verifyDelete()
+                    }
+                } label: {
+                    HStack {
+                        Text("YES, DELETE")
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .frame(height:50)
+                            .foregroundColor(.red)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                            .background(Color("UIpurple"))
+                            .cornerRadius(10)
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+}
 
 struct DeleteAccountUI: View {
     @StateObject private var viewModel = ProfileViewModelDeleteAcc()
@@ -139,7 +187,7 @@ struct DeleteAccountUI: View {
                                 do {
                                     try await viewModel.deleteAcc(emailP1:email1, emailP2:confirmEmail)
                                     isAuthenticated = true
-                                    //try viewModel.verifyDelete()
+                                    
                                 } catch deleteAccountErrors.notMatching {
                                     showingAlert = true
                                     alertMessage = "The two emails do not match"
@@ -176,6 +224,6 @@ struct DeleteAccountUI: View {
 
 struct Delete_Previews: PreviewProvider {
     static var previews: some View {
-        DeleteAccountUI()
+        ConfirmDelete()
     }
 }
