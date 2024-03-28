69ff972a42596fd1441100a6f5fd9d8570e784cb
Rishabh Pandey <email.rishabhp@gmail.com>
2024-03-27 21:28:35 -0400

    Image Caching Completed

--- OOTD-Swift/OOTD-Swift/Components/ImageLoader.swift
+++ OOTD-Swift/OOTD-Swift/Components/ImageLoader.swift
@@ -7,15 +7,26 @@
 
 import SwiftUI
 import Combine
+import UIKit
 
 class ImageLoader: ObservableObject {
     @Published var image: UIImage?
-    
+    private var cache: NSCache<NSURL, UIImage> = NSCache()
+
     func loadImage(from url: URL) {
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else { return }
+        if let cachedImage = cache.object(forKey: url as NSURL) {
             DispatchQueue.main.async {
-                self.image = UIImage(data: data)
+                self.image = cachedImage
+            }
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil, let downloadedImage = UIImage(data: data) else { return }
+            self?.cache.setObject(downloadedImage, forKey: url as NSURL) //caches image
+
+            DispatchQueue.main.async {
+                self?.image = downloadedImage
             }
         }.resume()
     }
